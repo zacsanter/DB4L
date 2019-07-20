ebdfbe5196ba01c0624cca980b936f3029167a2e
zacsanter <53116184+zacsanter@users.noreply.github.com>
2019-07-20 13:19:33 +0100

    Add files via upload

---
+++ intialise 2.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+    $(document).ready(function(){
+      $('.testimonial-slider').slick({
+        infinite: true,
+       slidesToShow: 3,
+        slidesToScroll: 3
+      });
+    });
+  </script>
+
