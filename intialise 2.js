e7a18eaef3b8c3d642b725cc31f5ec3b2c8fcf4a
zacsanter <53116184+zacsanter@users.noreply.github.com>
2019-07-20 13:22:42 +0100

    Update intialise 2.js

--- intialise 2.js
+++ intialise 2.js
@@ -1,10 +1,8 @@
-<script type="text/javascript">
-    $(document).ready(function(){
+$(document).ready(function(){
       $('.testimonial-slider').slick({
         infinite: true,
        slidesToShow: 3,
         slidesToScroll: 3
       });
     });
-  </script>
 
