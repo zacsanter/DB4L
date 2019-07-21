bf6fcc8ac0f1de67a74026064c5390fa610b47c0
zacsanter <53116184+zacsanter@users.noreply.github.com>
2019-07-21 12:14:16 +0100

    Create slick-script.js

---
+++ slick-script.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($){
+      $('.testimonial-slider').slick({
+      lazyLoad: 'ondemand'
+        infinite: true,
+       slidesToShow: 3,
+        slidesToScroll: 3
+      });
+      });
