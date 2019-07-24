e6f884705ccb8cddb0d8b49eedf97f06d1d064c1
zacsanter <53116184+zacsanter@users.noreply.github.com>
2019-07-24 17:21:29 +0100

    Update slick-script.js

--- slick-script.js
+++ slick-script.js
@@ -2,7 +2,26 @@ jQuery(document).ready(function($){
       $('.testimonial-slider').slick({
       lazyLoad: 'ondemand'
         infinite: true,
+         dots: true,
        slidesToShow: 3,
         slidesToScroll: 3
+        responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+ ]
       });
       });
