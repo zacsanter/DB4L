b2bb57fa61fd9b66bb61e997618f2cf2a8a2d1d9
zacsanter <53116184+zacsanter@users.noreply.github.com>
2019-07-25 13:47:03 +0100

    Update sliderr-script.js

--- sliderr-script.js
+++ sliderr-script.js
@@ -3,14 +3,14 @@ $('.testimonial-slider').slick({
   dots: true,
   infinite: false,
   speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
+  slidesToShow: 3,
+  slidesToScroll: 3,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 2,
+        slidesToScroll: 2,
         infinite: true,
         dots: true
       }
@@ -18,8 +18,8 @@ $('.testimonial-slider').slick({
     {
       breakpoint: 600,
       settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToShow: 1,
+        slidesToScroll: 1
       }
     },
     {
