288d993af340fac54998d0b014656ec60935a1b1
zacsanter <53116184+zacsanter@users.noreply.github.com>
2019-07-25 15:25:51 +0100

    Create DB4l.js

---
+++ DB4l.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function($){
+$('.testimonial-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    }
+  ]
+});
+ });
